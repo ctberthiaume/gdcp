9ced5ba7e983d719246b8571d55e144d50fdfdcf
Chris Berthiaume <chrisbee@uw.edu>
2015-10-22 16:02:46 -0700

    URL parsing for another form of Drive/Doc URL

--- test_gdcp.py
+++ test_gdcp.py
@@ -8,23 +8,29 @@ class GDCPTest(unittest.TestCase):
         Test ID parsing function
         """
         ids = [
-            "https://drive.google.com/drive/#folders/0B01234567890123456789012345",
-            "https://drive.google.com/drive/#folders/0B01234567890123456789012345/0B01234567890123456789012346",
-            "987012345/0B01234567890123456789012346",
-            "https://drive.google.com/open?id=0B01234567890123456789012346&authuser=0",
-            "https://drive.google.com/open?id=0B01234567890123456789012346",
-            "https://docs.google.com/a/uw.edu/file/d/0B1vFU7eD_alGaXRiMk1qa1J6UzQ/edit?usp=drivesdk",
-            "0B1vFU7eD_alGaXRiMk1qa1J6UzQ",
+            "https://drive.google.com/drive/#folders/0B01234567890123456789012340",
+            "https://drive.google.com/drive/#folders/0B01234567890123456789012340/0B01234567890123456789012341",
+            "987012345/0B01234567890123456789012342",
+            "https://drive.google.com/open?id=0B01234567890123456789012343",
+            "https://drive.google.com/open?id=0B01234567890123456789012344&authuser=0",
+            "https://docs.google.com/a/uw.edu/file/d/0B01234567890123456789012345/edit?usp=drivesdk",
+            "https://docs.google.com/a/uw.edu/file/d/0B01234567890123456789012346",
+            "https://drive.google.com/a/uw.edu/file/d/0B01234567890123456789012347/view?usp=drivesdk",
+            "https://drive.google.com/a/uw.edu/file/d/0B01234567890123456789012348",
+            "0B01234567890123456789012349",
             "not_an_id"
         ]
         correct_answers = [
+            "0B01234567890123456789012340",
+            "0B01234567890123456789012341",
+            "0B01234567890123456789012342",
+            "0B01234567890123456789012343",
+            "0B01234567890123456789012344",
             "0B01234567890123456789012345",
             "0B01234567890123456789012346",
-            "0B01234567890123456789012346",
-            "0B01234567890123456789012346",
-            "0B01234567890123456789012346",
-            "0B1vFU7eD_alGaXRiMk1qa1J6UzQ",
-            "0B1vFU7eD_alGaXRiMk1qa1J6UzQ",
+            "0B01234567890123456789012347",
+            "0B01234567890123456789012348",
+            "0B01234567890123456789012349",
             "not_an_id"
         ]
         answers = [gdcp.find_id(x) for x in ids]
